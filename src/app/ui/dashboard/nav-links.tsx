c6ca209681f0e1787052b9f20186d3d5aeeeeaaa
Surfsol <surfsol@protonmail.com>
2024-12-06 06:23:50 -0300

    add sidenav

--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -1,10 +1,9 @@
-
 'use client';
-// import {
-//   UserGroupIcon,
-//   HomeIcon,
-//   DocumentDuplicateIcon,
-// } from '@heroicons/react/24/outline';
+import {
+  UserGroupIcon,
+  HomeIcon,
+  AcademicCapIcon,
+} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'; //to get pathname from url
 import clsx from 'clsx'; //use for conditional rendering styles
@@ -12,13 +11,13 @@ import clsx from 'clsx'; //use for conditional rendering styles
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: null },
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
     name: 'Level Test',
     href: '/dashboard/level-test',
-    icon: null,
+    icon: AcademicCapIcon,
   },
-  { name: 'Lesson', href: '/dashboard/lesson', icon: null },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
@@ -35,11 +34,11 @@ export default function NavLinks() {
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
-              },
+              }
             )}
           >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <LinkIcon className='w-6' />
+            <p className='hidden md:block'>{link.name}</p>
           </Link>
         );
       })}
