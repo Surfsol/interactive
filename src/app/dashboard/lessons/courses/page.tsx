71119be456a45b946ec4c42c967837c3fd18f8a2
Surfsol <surfsol@protonmail.com>
2024-12-07 04:49:02 -0300

    add lesson

---
+++ src/app/dashboard/lessons/courses/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { useState } from 'react';
+
+export default function Courses() {
+    const [lessonStatus, setLessonStatus] = useState()
+  const nextLesson = '';
+  const lessonHistory = ''
+  return (
+    <main>
+      <div>Next Lesson ${nextLesson}</div>
+      <div>${lessonStatus}</div>
+      <div>${lessonHistory}</div>
+    </main>
+  );
+}
---
+++ src/app/dashboard/lessons/page.tsx
@@ -0,0 +1,15 @@
+
+
+import Courses from './courses/page';
+import CourseMaterial from './course-material/page';
+
+export default async function Lessons() {
+ 
+  return (
+    <main>
+      <div><Courses /></div>  
+      <div><CourseMaterial /></div>  
+
+    </main>
+  );
+}
